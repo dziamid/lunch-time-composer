4c3ff5856ac48294191af3ccf179740c40ee424c
Dziamid <dziamid@gmail.com>
2012-07-06 16:32:15 +0300

    fixed js deps and assetic inheritance

---
+++ src/LunchTime/ComposerHooks.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace LunchTime;
+
+class ComposerHooks
+{
+    public static function buildJavascripts($event)
+    {
+        //TODO: find out a package install dir
+        //execute jquery make and knockout build scripts
+
+    }
+
+}
